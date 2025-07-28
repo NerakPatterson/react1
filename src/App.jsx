bb9dc18eb3abc08717384c7421f949404bf2d5d4
NerakPatterson <nerak.patterson@gmail.com>
2025-07-27 19:10:30 -0700

    fixed netlify issue and background error

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import {useState} from 'react'
 import './App.css'
 import ProductList from './Components/ProductList';
 
+
 function App() {
   const [products] = useState([
-    {id: 1, name: "MDR Unisex Logo Tee", price: "19.99", description: "100% ring-spun cotton"},
-    {id: 2, name: "MDR Logo Beanie", price: "19.99", description: "Versatile Kniy Beanie"},
-    {id: 3, name: "MDR Logo Snapback", price: "29.99", description: "Flat Bill Snapback"},
-    {id: 4, name: "MDR Unisex Logo Hoodie", price: "39.99", description: "50% Cotton/%50 Polyester Champion Hoodie"},
-    {id: 5, name: "MDR Unisex Logo Tank", price: "19.99", description: "100% ring-spun cotton"},
+    {id: 1, name: "MDR Unisex Logo Tee", price: "$19.99", description: "100% ring-spun cotton"},
+    {id: 2, name: "MDR Logo Beanie", price: "$19.99", description: "Versatile Knit Beanie"},
+    {id: 3, name: "MDR Logo Snapback", price: "$29.99", description: "Flat Bill Snapback"},
+    {id: 4, name: "MDR Unisex Logo Hoodie", price: "$39.99", description: "50% Cotton/50% Polyester Champion Hoodie"},
+    {id: 5, name: "MDR Unisex Logo Tank", price: "$19.99", description: "100% ring-spun cotton"},
   ]);
   
   return (
@@ -18,4 +19,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
